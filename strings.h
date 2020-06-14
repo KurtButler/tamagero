7903b715953829b9af1db68862697dba7bc73b71
guangdongzoo <62613188+guangdongzoo@users.noreply.github.com>
2020-06-14 19:25:32 -0400

    Create strings.h

---
+++ strings.h
@@ -0,0 +1,32 @@
+
+//  Tamagero Names
+const String tamanames[] PROGMEM = { //Length 10 strings
+"Shokero   ", //No.0
+"Kikimono  ",
+"Shirogato ",
+"Kurogato  ",
+"Lilparca  ",
+
+"Niubi     ",
+"Pumpmo    ",
+"Bongos    ",
+"Tedibea   ",
+"Nillin    ",
+
+"Guff      ",//No.10
+"Same-Same ",
+"Bween     ",
+"Stackmo   ",
+"Boshimole ",
+"Kenda     ",
+"Buroku    ",
+"Azrael    ",
+"D.O.N.G.  ",
+"Mme Pinchy",
+
+"Arbusto   ",//No.20
+"Orion     ",
+"Ikumi     ",
+"Osuo      ",
+"Yrettab   ",
+"Same-Same " };
