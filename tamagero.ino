8df3d3336846d915ba69cdda37d1c01aec2488f9
guangdongzoo <62613188+guangdongzoo@users.noreply.github.com>
2020-06-15 10:42:43 -0400

    Release Version 1.0
    Removed unused code, balanced gameplay and timing, and graphics positioning.

--- tamagero.ino
+++ tamagero.ino
@@ -1,17 +1,13 @@
-
-/* Tamagero
+/* Tamagero v.1.0
     by Kurt Butler
 
-    Yes, this is a Tamagotchi clone. Made as an example project
+    Yes, this is a Tamagotchi clone. Made for fun
     on the Arduboy platform.
 */
-
 #include <Arduboy2.h>
 Arduboy2 ab;
 #include "images.h"
 #include "strings.h"
-
-
 
 // = = = = = = = = = = Variables = = = = = = = = = = = = =
 // Program Control
@@ -22,7 +18,7 @@ int MenuNextSt = 0;//Menu state to be transitioned to after half an A-press.
 
 // Timing variables
 int Twait = 300; //Time to wait after pressing a button.
-int eggagemax = 100; //Max egg age before hatching
+int eggagemax = 900; //Max egg age before hatching
 int aniTimer = 60;
 int aniSt = 0;
 int aniDir = 1;
@@ -32,7 +28,7 @@ int xpcntr = 0;
 // Player variables
 int xp = 0;
 int txp = 0;
-int money = 1000;
+int money = 10;
 bool dex[25];
 bool enLED = false; //Enable LED indicator for Tamagero health
 
@@ -42,14 +38,14 @@ float bored  = 0;
 float dirty  = 0;
 int level  = 0;
 bool alive = true;
-bool egg   = false; // make sure this is true before running
+bool egg   = true; // make sure this is true before running
 int age = 0;
 int tamaid = 1;
 
 // Other game variables
-float hmax = 6000.0; //Max hunger level before running away
-float dmax = 8500.0; //Max dirty level befroe running away
-float bmax = 5000.0; //Max boredom level
+float hmax = 40000.0; //Max hunger level before running away
+float dmax = 55000.0; //Max dirty level befroe running away
+float bmax = 12000.0; //Max boredom level
 int L = 10;  //temp
 const int8_t sprite[32] PROGMEM = {
 0x1C, 0x38, 0x30, 0x0C, 0x38, 0x1C, 0x1E, 0x78,
@@ -59,8 +55,7 @@ const int8_t sprite[32] PROGMEM = {
 
 
 
-// -  - - - - - - - - -  - - - - - - - - - - - - - - - - - - - -
-
+// -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 int dialogue(String string) {
   ab.setCursor(0,0);
   ab.print("\n!!");
@@ -69,10 +64,8 @@ int dialogue(String string) {
   ab.display();
 
   ab.pollButtons();
-  while (!ab.pressed(A_BUTTON)) {
-    ab.pollButtons();
-  }
-
+  delay(Twait);
+  while (!ab.pressed(A_BUTTON)) { ab.pollButtons(); }
   return 0;
 }
 
@@ -82,40 +75,12 @@ void setID(int id) {
   dex[id] = true;
 }
 
-void saveGame() {
-uint16_t address = 256;
-EEPROM.put(address, dex); address += sizeof(dex);
-EEPROM.put(address, hungry); address += sizeof(hungry);
-EEPROM.put(address, bored); address += sizeof(bored);
-EEPROM.put(address, dex); address += sizeof(dex);
-EEPROM.put(address, level); address += sizeof(level);
-EEPROM.put(address, xp); address += sizeof(xp);
-EEPROM.put(address, txp); address += sizeof(txp);
-EEPROM.put(address, money); address += sizeof(money);
-EEPROM.put(address, tamaid); address += sizeof(tamaid);
-EEPROM.put(address, alive); address += sizeof(alive);
-EEPROM.put(address, egg); address += sizeof(egg);
-EEPROM.put(address, age); address += sizeof(age);
-EEPROM.put(address, enLED); address += sizeof(enLED);
-//EEPROM.put(address, ); address += sizeof();
+void eggReset() {
+    hungry = 0; bored = 0; dirty =0; xp = 0;
+    age = 0; egg = true; alive = true;
+    eggagemax = 600+(rand()%400);
 }
 
-void loadGame() {
-uint16_t address = 256;
-EEPROM.get(address, dex); address += sizeof(dex);
-EEPROM.get(address, hungry); address += sizeof(hungry);
-EEPROM.get(address, bored); address += sizeof(bored);
-EEPROM.get(address, dex); address += sizeof(dex);
-EEPROM.get(address, level); address += sizeof(level);
-EEPROM.get(address, xp); address += sizeof(xp);
-EEPROM.get(address, txp); address += sizeof(txp);
-EEPROM.get(address, money); address += sizeof(money);
-EEPROM.get(address, tamaid); address += sizeof(tamaid);
-EEPROM.get(address, alive); address += sizeof(alive);
-EEPROM.get(address, egg); address += sizeof(egg);
-EEPROM.get(address, age); address += sizeof(age);
-EEPROM.get(address, enLED); address += sizeof(enLED);
-}
 
 // <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <> <>
 //                    Setup Program Code
@@ -130,7 +95,7 @@ void setup() {
 
   // My splash
   ab.setTextColor(WHITE);
-  ab.print("Tamagero Ver. 0.4\nKurt Butler");
+  ab.print("Tamagero Ver. 1.0\nKurt Butler");
   ab.display();
 
   digitalWrite(RED_LED, LOW);
@@ -140,16 +105,11 @@ void setup() {
   // ============= Game Init! ==================
   // Initialize Tamadex
   for (int i=0; i<25; i++) {dex[i] = false;}
-  setID(0);//rand()%25;
- 
-  // Load game
-  //loadGame();
+  setID(0);
  
   delay(1000);
   ab.clear();
   ab.display();
-
-  //GameSt=0;
 }
 
 
@@ -192,9 +152,9 @@ void loop() {
       } else {
         // EXP System
         if (xpcntr <= 0) {
-          xp++; txp++; xpcntr = 300;
-          if (bored <= 0.1*bmax) {money++;}
-          if (xp > level*10 + 0.1*xp) {level++; money = money+10+level;}
+          xp++; txp++; xpcntr = 100;
+          if (bored <= 0.05*bmax ) {xp++;}
+          if (xp > 10 + level*20) {level++; money = money+2 +level;}
         } else {
           xpcntr--;
         }
@@ -215,24 +175,23 @@ void loop() {
     digitalWrite(BLUE_LED, LOW);
   }
   else {
-    if (hungry > 0.9 * hmax || dirty > 0.9 * hmax) {//LED Blue
+    if (hungry > 0.9 * hmax || dirty > 0.9 * hmax) {//LED Red
       digitalWrite(RED_LED, LOW);
       digitalWrite(GREEN_LED, HIGH);
       digitalWrite(BLUE_LED, HIGH);
-    } //LED red
-    else if (hungry > 0.5 * hmax || dirty > 0.5 * hmax) {//LED Blue
+    } 
+    else if (hungry > 0.5 * hmax || dirty > 0.5 * hmax) {//LED Yellow
       digitalWrite(RED_LED, LOW);
       digitalWrite(GREEN_LED, LOW);
       digitalWrite(BLUE_LED, HIGH);
-    } //LED yellow
-    else {//LED Blue
+    } 
+    else {//LED Green
       digitalWrite(RED_LED, HIGH);
       digitalWrite(GREEN_LED, LOW);
       digitalWrite(BLUE_LED, HIGH);
-    }//LED green
+    }
   }
 
-  //ApplyProgmemMenuAction(player, EquipmentMenu, menuIndex);
 
   // = = = = = Menu Control Logic = = = = =
   // Main Menu
@@ -242,7 +201,6 @@ void loop() {
       if (MenuSel==0) {MenuNextSt = 2;}
       if (MenuSel==1) {MenuNextSt = 1;}
       if (MenuSel==2) {enLED = !enLED; delay(Twait);}
-      //if (MenuSel==3) {saveGame(); delay(Twait); dialogue("Game was successfully\nsaved.");}
       }
     else if (ab.pressed(B_BUTTON)) { age = age; }
     else if (ab.pressed(UP_BUTTON) && MenuSel > 0) { MenuSel--; delay(Twait);}
@@ -253,16 +211,35 @@ void loop() {
   if (MenuSt == 2) {
     if (ab.pressed(A_BUTTON)) {
       if (MenuSel==0) { // FEED TAMAGERO
-        //                            #####################
         if (bored == bmax) {dialogue(F("Your tamagero refused\nto eat!"));} // Only if not starving
-        else {hungry = 0;} //Maybe make this incremental ?
+        else {
+          if (money>=1) {
+            money = money-1;
+            hungry = hungry - 0.33*hmax;
+            if (hungry < 0) {hungry=0;}
+            delay(Twait);
+          } else {dialogue(F("You don't have enough\nmoney!"));}
+          } 
       }
       if (MenuSel==1) { // CLEAN TAMAGERO
         if (bored == bmax) {dialogue(F("Your tamagero refused\nto bathe!"));}
-        else {dirty = 0;}
+        else {
+            if (money>=1) {
+              money = money-1;
+              dirty = dirty - 0.33*dmax;
+              if (dirty < 0) { dirty = 0; }
+              delay(Twait);
+            } else {dialogue(F("You don't have enough\nmoney!"));}
+          }
       }
-      if (MenuSel==2) {bored = 0;}
-      if (MenuSel==3) {//age = 0; egg = true; alive = true;
+      if (MenuSel==2) { // PLAY
+        // - - -
+          bored = bored - 0.33*bmax;
+          if (bored < 0) { bored = 0; }
+          delay(Twait);
+        // - - -
+        }
+      if (MenuSel==3) {
         MenuNextSt = 5;
       }
     }
@@ -293,13 +270,11 @@ void loop() {
   if (MenuSt == 5) {
     if (ab.pressed(B_BUTTON)) { MenuNextSt = 2; delay(Twait); }
     else if (ab.pressed(A_BUTTON)) {
-        //                #####################
         if (MenuSel==0) {//Buy random
           if (money>=10) {
             money = money-10;
             setID(rand()%25);
-            hungry = 0; bored = 0; dirty =0;
-            age = 0; egg = true; alive = true;
+            eggReset();
             MenuNextSt = 0;
           } else {dialogue(F("You don't have enough\nmoney!"));}
         }
@@ -312,8 +287,7 @@ void loop() {
               else {break;}
             }
             setID(L);
-            hungry = 0; bored = 0; dirty =0;
-            age = 0; egg = true; alive = true;
+            eggReset();
             MenuNextSt = 0;
           } else {dialogue(F("You don't have enough\nmoney!"));}
         }
@@ -328,7 +302,6 @@ void loop() {
 
   // <> <> <> <> <> <> <> <> <>  Update graphics <> <> <> <> <> <> <> <> <>
   ab.clear();
-  //ab.drawSlowXYBitmap(50, 10, (tama0), 16, 16, WHITE);
   ab.drawSlowXYBitmap(0, 0, (tamabg), 128,64, WHITE);
   L =22-(22*hungry)/hmax;
   ab.fillRect(23,5,  L,  3,WHITE); //FOOD   (max pix is 22)    // LATER: Make sure these use saturated arithmetic
@@ -350,14 +323,13 @@ void loop() {
         else if (aniSt==4) {aniSt=5; aniTimer=1;  aniX--;}
         else if (aniSt==5) {aniSt=0; aniTimer=40 +  (rand() % 30);  aniX--; aniDir = 2*(rand()%2)-1;}
       } else {aniTimer--;}
-      ab.drawSlowXYBitmap(75+aniDir*aniX, 15-(aniSt%2), (ntamablock + 32*tamaid), 16, 16, BLACK);
+      ab.drawSlowXYBitmap(80+aniDir*aniX, 15-(aniSt%2), (ntamablock + 32*tamaid), 16, 16, BLACK);
     }
   }
 
  
   // <> <> <> <> <> <> <> <> <> Menu Text <> <> <> <> <> <> <> <> <> <>
   ab.setCursor(0,32);
-  //ab.print("  Actions\n* Statistics\n  Sleep\n  Settings");
   if (MenuSt == 0) {// Main Menu
     if (MenuSel==0) {ab.print("*");} else {ab.print(" ");}
     ab.print(F(" Actions\n"));
@@ -365,18 +337,9 @@ void loop() {
     ab.print(F(" Statistics\n"));
     if (MenuSel==2) {ab.print("*");} else {ab.print(" ");}
     ab.print(F(" Toggle LED\n"));
-    
-    //if (MenuSel==3) {ab.print("*");} else {ab.print(" ");}
-    //ab.print(" Save Game\n");
-//    if (MenuSel==3) {ab.print("*");} else {ab.print(" ");}
-//ab.print(aniTimer);
-//ab.print("|");
-//ab.print(aniSt);
-//    ab.print(" Settings");
   }
   if (MenuSt == 2) {// Action Menu
     if (MenuSel==0) {ab.print("*");} else {ab.print(" ");}
-    //      #####################
     ab.print(F(" Feed  ($1)    $"));
     ab.print(money);
     ab.print("\n");
@@ -397,15 +360,14 @@ void loop() {
   if (!egg) {
     ab.print(tamanames[(tamaid % 25)]);
   } else {ab.print("???");}
-    ab.print(" \nAge:");
+    ab.print(" \nAge: ");
     ab.print((((age)/30.0)/3600.0),3);
-    ab.print(F(" hr\nXp: "));
+    ab.print(F(" hr\nXp:  "));
     ab.print(xp);
-    ab.print(F("  \nLvl:"));
+    ab.print(F("  \nLvl: "));
     ab.print(level);
   }
   if (MenuSt==4) {  //   Player Info
-    //        #####################
     ab.print(F("Total Xp: "));
     ab.print(txp);
     ab.print(F("\nMoney:    "));
@@ -417,13 +379,10 @@ void loop() {
       if (dex[i]) {ab.print("o");}
       else {ab.print("-");}
       if (i==12) {ab.print("\n    ");}
-      //if ((i%4==3)&(i<16)) {ab.print("  ");}
-      //if ((i%4==0)&(i>16)) {ab.print("  ");}
     }
   }
   if (MenuSt == 5) { // Buy Tamagero Menu
     if (MenuSel==0) {ab.print("*");} else {ab.print(" ");}
-    //       #####################
     ab.print(F(" Buy any ($10)  $"));
     ab.print(money);
     ab.print("\n");
@@ -431,16 +390,5 @@ void loop() {
     ab.print(F(" Buy new ($20)\n"));
   }
  
-  //ab.print("  (");
-  //ab.print(MenuSt);
-  //ab.print(",");
-  //ab.print(MenuSel);
-  //ab.print(") &");
-  //ab.print(tamaid);
-  //ab.print(L);
   ab.display();
-
-  if (ab.pressed(RIGHT_BUTTON)) {setID((tamaid+1)%25); delay(Twait);}
-  if (ab.pressed(LEFT_BUTTON))  {setID((tamaid+24)%25); delay(Twait);}
- 
 }
